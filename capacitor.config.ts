0469c0a1dacbb9a7bf531a603795e5cdb58c94af
Elkin Omar Jimenez Garcia <jimenezelkg+GHCO@hitss.com>
2026-09-23 08:04:17 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Food-Planner-App',
+  webDir: 'www'
+};
+
+export default config;
